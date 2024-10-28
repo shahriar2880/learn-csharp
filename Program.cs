ec7ec095dd7995abf29269f788efdf058ca00fdf
shahriar2880 <shahriarreza436@gmail.com>
2024-10-29 04:58:55 +0600

    variable and datatypes

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Test{
+    public static void Main(string[] args){
+        // string firstName = "Shahriar";
+        // int age = 26;
+        // double cgpa =  3.46 ;
+        // char bloodgroup = 'O';
+        // bool isAdmitted = true;
+        // Console.WriteLine( firstName);
+        // Console.WriteLine(age);
+        // Console.WriteLine( cgpa);
+        // Console.WriteLine( bloodgroup);
+        // Console.WriteLine( isAdmitted);
+    }
+}
